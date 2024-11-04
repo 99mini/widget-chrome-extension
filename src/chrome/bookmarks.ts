af16eb6b0cbf26ba628c6e9fc553ec4892a5a9c4
mimi99 <0mini9939@gmail.com>
2024-11-05 04:48:04 +0900

    refactor: Refactor bookmark.ts to use async functions
    - Rename the exported functions in bookmark.ts to use the async function syntax for better readability and consistency.

--- src/chrome/bookmarks.ts
+++ src/chrome/bookmarks.ts
@@ -1,4 +1,4 @@
-export const getTree = async (): Promise<chrome.bookmarks.BookmarkTreeNode[]> => {
+export async function getTree(): Promise<chrome.bookmarks.BookmarkTreeNode[]> {
   return new Promise((resolve, reject) => {
     chrome.bookmarks.getTree((bookmarkTreeNodes) => {
       resolve(bookmarkTreeNodes);
@@ -7,9 +7,9 @@ export const getTree = async (): Promise<chrome.bookmarks.BookmarkTreeNode[]> =>
       }
     });
   });
-};
+}
 
-export const search = async (query: string): Promise<chrome.bookmarks.BookmarkTreeNode[]> => {
+export async function search(query: string): Promise<chrome.bookmarks.BookmarkTreeNode[]> {
   return new Promise((resolve, reject) => {
     chrome.bookmarks.search(query, (bookmarkTreeNodes) => {
       resolve(bookmarkTreeNodes);
@@ -18,11 +18,9 @@ export const search = async (query: string): Promise<chrome.bookmarks.BookmarkTr
       }
     });
   });
-};
+}
 
-export const create = async (
-  bookmark: chrome.bookmarks.BookmarkCreateArg
-): Promise<chrome.bookmarks.BookmarkTreeNode> => {
+export async function create(bookmark: chrome.bookmarks.BookmarkCreateArg): Promise<chrome.bookmarks.BookmarkTreeNode> {
   return new Promise((resolve, reject) => {
     chrome.bookmarks.create(bookmark, (bookmarkTreeNode) => {
       resolve(bookmarkTreeNode);
@@ -31,12 +29,12 @@ export const create = async (
       }
     });
   });
-};
+}
 
-export const update = async (
+export async function update(
   id: string,
   changes: chrome.bookmarks.BookmarkChangesArg
-): Promise<chrome.bookmarks.BookmarkTreeNode> => {
+): Promise<chrome.bookmarks.BookmarkTreeNode> {
   return new Promise((resolve, reject) => {
     chrome.bookmarks.update(id, changes, (bookmarkTreeNode) => {
       resolve(bookmarkTreeNode);
@@ -45,9 +43,9 @@ export const update = async (
       }
     });
   });
-};
+}
 
-export const remove = async (id: string): Promise<void> => {
+export async function remove(id: string): Promise<void> {
   return new Promise((resolve, reject) => {
     chrome.bookmarks.remove(id, () => {
       resolve();
@@ -56,4 +54,16 @@ export const remove = async (id: string): Promise<void> => {
       }
     });
   });
-};
+}
+
+export async function move(id: string, newParentId: string): Promise<chrome.bookmarks.BookmarkTreeNode> {
+  return new Promise((resolve, reject) => {
+    chrome.bookmarks.move(id, { parentId: newParentId }, (bookmarkTreeNode) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(bookmarkTreeNode);
+      }
+    });
+  });
+}
