7518b334c5ba9267d8f1a2a88289a3be987c68c0
99mini <itman390@gmail.com>
2024-12-01 03:54:08 +0900

    feat: enhance CustomWidgetModal with Clock widget and improved layout

--- src/components/CustomWidgetModal.tsx
+++ src/components/CustomWidgetModal.tsx
@@ -1,10 +1,11 @@
+import styled from '@emotion/styled';
 import React from 'react';
 import { createPortal } from 'react-dom';
-import styled from '@emotion/styled';
 
 import { ModalBackground, ModalContainerCSS } from './Modal';
 
 import useClickAway from '@/hook/useClickAway';
+import Clock from '@/widget/Clock';
 
 const ModalContainer = styled.div`
   display: flex;
@@ -12,10 +13,30 @@ const ModalContainer = styled.div`
 
   gap: 8px;
 
-  width: 50%;
-  height: 50%;
+  width: 80%;
+  height: 80%;
+
+  padding: 24px;
 
   ${ModalContainerCSS}
+`;
+
+const WidgetList = styled.div`
+  display: grid;
+  gap: ${({ theme }) => `${theme.sizes.widget.columnGap}px ${theme.sizes.widget.rowGap}px `};
+  justify-content: center;
+
+  grid-template-columns: repeat(auto-fill, 60px);
+`;
+
+const ClickableWidget = styled.button<{ rowSpan?: boolean; colSpan?: boolean }>`
+  grid-row: ${({ rowSpan }) => (rowSpan ? 'span 2' : 'span 1')};
+  grid-column: ${({ colSpan }) => (colSpan ? 'span 2' : 'span 1')};
+
+  padding: 0;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 type CustomWidgetModalProps = {
@@ -27,7 +48,13 @@ const CustomWidgetModal: React.FC<CustomWidgetModalProps> = ({ onClose }) => {
 
   return createPortal(
     <ModalBackground>
-      <ModalContainer ref={ref}></ModalContainer>
+      <ModalContainer ref={ref}>
+        <WidgetList>
+          <ClickableWidget rowSpan colSpan>
+            <Clock />
+          </ClickableWidget>
+        </WidgetList>
+      </ModalContainer>
     </ModalBackground>,
     document.body
   );
