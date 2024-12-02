4b078507865ecb190fcac65db1f394da3379cee7
99mini <itman390@gmail.com>
2024-12-02 16:51:12 +0900

    feat: enhance widget layout responsiveness; add media query generation for dynamic column sizing

--- src/widget/WidgetLayout.tsx
+++ src/widget/WidgetLayout.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-type WidgetLayoutProps = {
-  children: React.ReactNode;
-};
+function generateMediaQueryLayout(base: number, unit: number, cell: number, maxColumns = 20) {
+  let css = ``;
 
+  for (let i = 1; i <= maxColumns / 4; i++) {
+    css += `@media (min-width: ${base + unit * (i - 1)}px) and (max-width: ${base + unit * i}px) { grid-template-columns: repeat(${i * 4}, ${cell}px); }`;
+  }
+
+  return css;
+}
+
+/*
+  media breakpoints:
+  4 columns size: 384px (unit)
+
+  456px: 4 columns (base)
+  840px: 8 columns (base + unit)
+  1224px: 12 columns (base + 2 * unit)
+  1608px: 16 columns (base + 3 * unit)
+  1992px: 20 columns (base + 4 * unit)
+  2376px: 24 columns (base + 5 * unit)
+  ...
+*/
 const Container = styled.div`
   display: grid;
   justify-content: center;
@@ -13,8 +31,14 @@ const Container = styled.div`
 
   padding: 36px;
 
-  grid-template-columns: repeat(auto-fill, 60px);
+  grid-template-columns: repeat(auto-fill, ${({ theme }) => theme.sizes.widget.icon}px);
+
+  ${({ theme }) => generateMediaQueryLayout(456, 384, theme.sizes.widget.icon)}
 `;
+
+type WidgetLayoutProps = {
+  children: React.ReactNode;
+};
 
 const WidgetLayout: React.FC<WidgetLayoutProps> = ({ children }) => {
   return <Container>{children}</Container>;
