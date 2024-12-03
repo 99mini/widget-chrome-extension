0527fc078b08c4839e178f2bbe3513ad75fccb6c
99mini <itman390@gmail.com>
2024-12-03 15:19:23 +0900

    feat: update icon path retrieval; adjust paths for icons based on environment and improve compatibility with Chrome extensions

--- .config/vitest.ts
+++ .config/vitest.ts
@@ -1,1 +1,4 @@
 import '@testing-library/jest-dom';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).chrome = undefined;
--- __test__/utils/icon.test.ts
+++ __test__/utils/icon.test.ts
@@ -5,12 +5,6 @@ describe('Icon utils', () => {
   it('should return correct path for 24px icon', () => {
     const path = getIconPath('widgets_24');
 
-    expect(path).toBe('./src/assets/widgets_24.svg');
-  });
-
-  it('should return correct path for 64px icon', () => {
-    const path = getIconPath('widgets_64');
-
-    expect(path).toBe('./src/assets/widgets_64.svg');
+    expect(path).toBe('assets/widgets_24.svg');
   });
 });
