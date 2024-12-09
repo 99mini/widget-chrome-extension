13dd9a1881411bfcfdd9e74432f53495f2046ef6
99mini <itman390@gmail.com>
2024-12-09 14:43:50 +0900

    fix: update popup file reference in manifest and build script

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -19,27 +19,13 @@ if [ "$MODE" = "production" ]; then
 
   # generate manifest.json
 
-  # {
-  #   "manifest_version": 3,
-  #   "name": "widget chrome extension",
-  #   "description": "A Chrome extension for a custom new tab. 새 탭 커스텀 크롬 익스텐션입니다.",
-  #   "version": "0.1.0",
-  #   "action": {
-  #     "default_popup": "index.html"
-  #   },
-  #   "permissions": ["storage", "bookmarks"],
-  #   "chrome_url_overrides": {
-  #     "newtab": "index.html"
-  #   }
-  # }
-
   echo "{
   \"manifest_version\": 3,
   \"name\": \"widget chrome extension\",
   \"description\": \"A Chrome extension for a custom new tab. 새 탭 커스텀 크롬 익스텐션입니다.\",
   \"version\": \"$version\",
   \"action\": {
-    \"default_popup\": \"index.html\"
+    \"default_popup\": \"popup.html\"
   },
   \"permissions\": [\"storage\", \"bookmarks\"],
   \"chrome_url_overrides\": {
