2c603bb58a18784e697946490b563911e3c24af7
99mini <itman390@gmail.com>
2024-12-23 20:15:39 +0900

    fix: enhance documentation for URL and color conversion utilities

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -1,8 +1,12 @@
 /**
- *
+ * @description Convert rgb color to hex color
  * @param rgb
  * @param opacity
  * @returns rgb = rgb(255, 255, 255), opacity = 0.5 => rgba(255, 255, 255, 0.5)
+ * @example
+ * ```ts
+ * rgbWithAlpha('rgb(255, 255, 255)', 0.5); // => rgba(255, 255, 255, 0.5)
+ * ```
  */
 export function rgbWithAlpha(rgb: string, opacity = 1) {
   return `rgba(${rgb.slice(4, -1)}, ${opacity})`;
@@ -10,9 +14,14 @@ export function rgbWithAlpha(rgb: string, opacity = 1) {
 
 /**
  *
+ * @description Convert hex color to rgba color
  * @param hex
  * @param opacity
  * @returns hex = #ffffff, opacity = 0.5 => rgba(255, 255, 255, 0.5)
+ * @example
+ * ```ts
+ * hexWithAlpha('#ffffff', 0.5); // => rgba(255, 255, 255, 0.5)
+ * ```
  */
 export function hexWithAlpha(hex: string, opacity = 1) {
   const rgb = hex.match(/\w\w/g)?.map((x) => parseInt(x, 16));
